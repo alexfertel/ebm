963f36bf5e95039c8bc4212fc67a8424475a6751
Alexander Gonzalez <alexfertel97@gmail.com>
2018-12-06 16:31:21 -0500

    Remove some unused code

--- src/server.py
+++ src/server.py
@@ -217,28 +217,6 @@ class EBMS(rpyc.Service):
             successors += remote_successors
         self.successors = tuple(successors)
 
-    # def am_i_online(self):
-    #     ifaces = ['eth0', 'wlan0']
-    #     connected = []
-    # 
-    #     i = 0
-    #     for ifname in ifaces:
-    #         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    #         try:
-    #             socket.inet_ntoa(fcntl.ioctl(
-    #                 s.fileno(),
-    #                 0x8915,  # SIOCGIFADDR
-    #                 struct.pack('256s', ifname[:15])
-    #             )[20:24])
-    #             connected.append(ifname)
-    #             # print "%s is connected" % ifname
-    #         except:
-    #             # print "%s is not connected" % ifname
-    #             pass
-    #         i += 1
-    # 
-    #     return connected
-
     # ##################################################### DATA ######################################################
     def exposed_get(self, key):
         succ = self.exposed_find_successor(key)
